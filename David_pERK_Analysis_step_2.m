164cb815aff0c8dc837b8a611bb20195e84e9b53
davidkow9999 <teck.kow@monash.edu>
2024-10-24 13:43:53 +0800

    Update David_pERK_Analysis_step_2.m

--- David_pERK_Analysis_step_2.m
+++ David_pERK_Analysis_step_2.m
@@ -205,51 +205,6 @@ end
 [int_ven_MKJNJ,int_dor_MKJNJ,iv_mj_all,id_mj_all] = sortvd(MKJNJ_xyz,MKJNJ_samples_planes);
 [int_ven_MKTAK,int_dor_MKTAK,iv_mt_all,id_mt_all] = sortvd(MKTAK_xyz,MKTAK_samples_planes);
 
-% LMM - v vs d
-% [lme_vd_DMSO,md_vd_DMSO] = fixmm(int_ven_DMSO,int_dor_DMSO);
-% p_value = lme_vd_DMSO.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_DMSO.Coefficients.Estimate(3));
-% p_log_vd_DMSO = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_JNJ,md_vd_JNJ] = fixmm(int_ven_JNJ,int_dor_JNJ);
-% p_value = lme_vd_JNJ.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_JNJ.Coefficients.Estimate(3));
-% p_log_vd_JNJ = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_TAK,md_vd_TAK] = fixmm(int_ven_TAK,int_dor_TAK);
-% p_value = lme_vd_TAK.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_TAK.Coefficients.Estimate(3));
-% p_log_vd_TAK = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_MK,md_vd_MK] = fixmm(int_ven_MK,int_dor_MK);
-% p_value = lme_vd_MK.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_MK.Coefficients.Estimate(3));
-% p_log_vd_MK = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_MKJNJ,md_vd_MKJNJ] = fixmm(int_ven_MKJNJ,int_dor_MKJNJ);
-% p_value = lme_vd_MKJNJ.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_MKJNJ.Coefficients.Estimate(3));
-% p_log_vd_MKJNJ = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_MKTAK,md_vd_MKTAK] = fixmm(int_ven_MKTAK,int_dor_MKTAK);
-% p_value = lme_vd_MKTAK.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_MKTAK.Coefficients.Estimate(3));
-% p_log_vd_MKTAK = -1 * log10(p_value) * es_sign;
-% 
-% figure('Name','Ventral vs dorsal difference at stimulated condition', 'visible','on'); clf;
-% plot([p_log_vd_DMSO,p_log_vd_JNJ,p_log_vd_TAK,p_log_vd_MK,p_log_vd_MKJNJ,p_log_vd_MKTAK],'LineWidth',1.5,'Color','blue','Marker','.','MarkerSize',15); hold on;
-% xticks([1,2,3,4,5,6]);
-% xticklabels({'DMSO','JNJ','TAK','MK','MKJNJ','MKTAK'});
-% xlim([0.5 6.5]);
-% ylim([0 24]);
-% ylabel('-log_{10}(p)','Interpreter', 'tex')
-% ax = gca;
-% ax.XAxis.FontSize = 13;
-% ax.YAxis.FontSize = 13;
-% ax.XAxis.LineWidth = 1.5;
-% ax.YAxis.LineWidth = 1.5;
-% ax.Box = "off";
-
 %% ven vs dorsal within group
 [h_d,p_d,~,stats_d] = ttest2(iv_d_all,id_d_all); 
 [h_j,p_j,~,stats_j] = ttest2(iv_j_all,id_d_all);
@@ -384,22 +339,6 @@ test_group = [ones(1,length(id_m_all))*1, ones(1,length(id_mj_all))*2, ones(1,le
 [p_dor_mjt,tbl_dor_mjt,stats_dor_mjt] = anova1(test_data,test_group','off');
 p_dor_mjt_bon = multcompare(stats_dor_mjt,"CriticalValueType","bonferroni");
 
-return
-
-% two-way anova (CANNOT because of perfect multicolinearity)
-% % DMSO JNJ TAK MK
-% % ventral
-% test_data_DMSO = [iv_d_all;iv_j_all;iv_t_all;iv_m_all];
-% test_group_DMSO = [ones(1,length(iv_d_all))*1, ones(1,length(iv_j_all))*2, ones(1,length(iv_t_all))*3, ones(1,length(iv_m_all))*4];
-% obj1_leng = sum([double(length(iv_d_all)),double(length(iv_j_all)),double(length(iv_t_all))]);
-% test_factor_DMSO(1:length(iv_d_all)) = "normal";
-% test_factor_DMSO(length(iv_d_all)+1:obj1_leng) = "enhance";
-% test_factor_DMSO(obj1_leng+1:obj1_leng+length(iv_m_all)) = "impair";
-% [~,~,stats_DMSO] = anovan(test_data_DMSO,{test_group_DMSO test_factor_DMSO},"Model",2, ...
-%     "Varnames",["test_group_DMSO", "test_factor_DMSO"]);
-% [results,~,~,gnames] = multcompare(stats_DMSO,"Dimension",[1 2]);
-
-
 % ven
 % [f_v_d,bc_v_d] = boxplot3(iv_d_all,iv_j_all,iv_t_all,'DMSO','JNJ','TAK','pERK/tERK ven DMSO'); hold on;
 % ylim([0 4])
@@ -454,128 +393,7 @@ return
 % plot([1.1,1.9],[3.5 3.5],'LineWidth',1.5,'Color',[0 0 0]);
 % text(1.3,3.55,'***','FontSize',24,'FontWeight','bold');
 
-
-
-% %% Linear mixed model - ven vs dor
-% % ventral
-% [lme_d_JNJ,md_v_JNJ] = fixmm(int_ven_JNJ,int_ven_DMSO);
-% p_value = lme_d_JNJ.Coefficients.pValue(3);
-% es_sign = sign(lme_d_JNJ.Coefficients.Estimate(3));
-% p_log_v_JNJ = -1 * log10(p_value) * es_sign;
-% 
-% [lme_d_TAK,md_v_TAK] = fixmm(int_ven_TAK,int_ven_DMSO);
-% p_value = lme_d_TAK.Coefficients.pValue(3);
-% es_sign = sign(lme_d_TAK.Coefficients.Estimate(3));
-% p_log_v_TAK = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_MK,md_v_MK] = fixmm(int_ven_MK,int_ven_DMSO);
-% p_value = lme_vd_MK.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_MK.Coefficients.Estimate(3));
-% p_log_v_MK = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_MKJNJ,md_v_MKJNJ] = fixmm(int_ven_MKJNJ,int_ven_MK);
-% p_value = lme_vd_MKJNJ.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_MKJNJ.Coefficients.Estimate(3));
-% p_log_v_MKJNJ = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_MKTAK,md_v_MKTAK] = fixmm(int_ven_MKTAK,int_ven_MK);
-% p_value = lme_vd_MKTAK.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_MKTAK.Coefficients.Estimate(3));
-% p_log_v_MKTAK = -1 * log10(p_value) * es_sign;
-% 
-% % dorsal
-% [lme_d_JNJ,md_d_JNJ] = fixmm(int_dor_JNJ,int_dor_DMSO);
-% p_value = lme_d_JNJ.Coefficients.pValue(3);
-% es_sign = sign(lme_d_JNJ.Coefficients.Estimate(3));
-% p_log_d_JNJ = -1 * log10(p_value) * es_sign;
-% 
-% [lme_d_TAK,md_d_TAK] = fixmm(int_dor_TAK,int_dor_DMSO);
-% p_value = lme_d_TAK.Coefficients.pValue(3);
-% es_sign = sign(lme_d_TAK.Coefficients.Estimate(3));
-% p_log_d_TAK = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_MK,md_d_MK] = fixmm(int_dor_MK,int_dor_DMSO);
-% p_value = lme_vd_MK.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_MK.Coefficients.Estimate(3));
-% p_log_d_MK = -1 * log10(p_value) * es_sign;
-% 
-% [lme_vd_MKJNJ,md_d_MKJNJ] = fixmm(int_dor_MKJNJ,int_dor_MK);
-% p_value = lme_vd_MKJNJ.Coefficients.pValue(3);
-% es_sign = sign(lme_vd_MKJNJ.Coefficients.Estimate(3));
-% p_log_d_MKJNJ = -1 * log10(p_value) * es_sign;
-% 
-% [lme_d_MKTAK,md_d_MKTAK] = fixmm(int_dor_MKTAK,int_dor_MK);
-% p_value = lme_d_MKTAK.Coefficients.pValue(3);
-% es_sign = sign(lme_d_MKTAK.Coefficients.Estimate(3));
-% p_log_d_MKTAK = -1 * log10(p_value) * es_sign;
-% 
-% cg_mat = [...
-%     p_log_v_JNJ,p_log_d_JNJ;...
-%     p_log_v_TAK,p_log_d_TAK;...
-%     p_log_v_MK,p_log_d_MK;...
-%     p_log_v_MKJNJ,p_log_d_MKJNJ;...
-%     p_log_v_MKTAK,p_log_d_MKTAK;];
-% 
-% cg = clustergram(cg_mat);
-% cg.ColumnLabels = {'ventral','dorsal'};
-% cg.RowLabels = {'JNJ','TAK','MK','MKJNJ','MKTAK'};
-% cg.DisplayRange = 3;
-% cg.ShowDendrogram = 'off';
-% 
-% %% Linear mixed model -- habenula as a whole
-% % DMSO_pt_all = [DMSO_perk_terk{1};DMSO_perk_terk{2};DMSO_perk_terk{3}];
-% % MK_pt_all = [MK_perk_terk{1};MK_perk_terk{2};MK_perk_terk{3}];
-% % MKJNJ_pt_all = [MKJNJ_perk_terk{1};MKJNJ_perk_terk{2};MKJNJ_perk_terk{3}];
-% % MKTAK_pt_all = [MKTAK_perk_terk{1};MKTAK_perk_terk{2};MKTAK_perk_terk{3}];
-% % TAK_pt_all = [TAK_perk_terk{1};TAK_perk_terk{2};TAK_perk_terk{3}];
-% % JNJ_pt_all = [JNJ_perk_terk{1};JNJ_perk_terk{2};JNJ_perk_terk{3}];
-% % 
-% % beta_MK = mean(MK_pt_all,'all') - mean(DMSO_pt_all,'all');
-% % beta_MKTAK = mean(MKJNJ_pt_all,'all') - mean(DMSO_pt_all,'all');
-% % beta_MKJNJ = mean(MKTAK_pt_all,'all') - mean(DMSO_pt_all,'all');
-% % beta_TAK = mean(JNJ_pt_all,'all') - mean(DMSO_pt_all,'all');
-% % beta_JNJ = mean(TAK_pt_all,'all') - mean(DMSO_pt_all,'all');
-% % 
-% % neu_int = [DMSO_pt_all;MK_pt_all;MKJNJ_pt_all;MKTAK_pt_all;JNJ_pt_all;TAK_pt_all];
-% % grp_varia = [ones(1,length(DMSO_pt_all))*1,ones(1,length(MK_pt_all))*2,ones(1,length(MKJNJ_pt_all))*3,...
-% %     ones(1,length(MKTAK_pt_all))*4,ones(1,length(TAK_pt_all))*5,ones(1,length(JNJ_pt_all))*6];
-% % 
-% % grp_beta = [ones(1,length(DMSO_pt_all))*1,ones(1,length(MK_pt_all))*beta_MK,ones(1,length(MKJNJ_pt_all))*beta_MKJNJ,...
-% %     ones(1,length(MKTAK_pt_all))*beta_MKTAK,ones(1,length(TAK_pt_all))*beta_TAK,ones(1,length(JNJ_pt_all))*beta_JNJ];
-% % 
-% % grp_date = [ones(1,length(DMSO_pt_all))*1,ones(1,length(MK_pt_all))*2,ones(1,length(MKJNJ_pt_all))*3,...
-% %     ones(1,length(MKTAK_pt_all))*3,ones(1,length(TAK_pt_all))*4,ones(1,length(JNJ_pt_all))*1];
-% 
-% [lme_JNJ,md_JNJ] = fixmm(JNJ_samples_planes,DMSO_samples_planes);
-% p_value = lme_JNJ.Coefficients.pValue(3);
-% es_sign = sign(lme_JNJ.Coefficients.Estimate(3));
-% p_log_JNJ = -1 * log10(p_value) * es_sign;
-% 
-% [lme_TAK,md_TAK] = fixmm(TAK_samples_planes,DMSO_samples_planes);
-% p_value = lme_TAK.Coefficients.pValue(3);
-% es_sign = sign(lme_TAK.Coefficients.Estimate(3));
-% p_log_TAK = -1 * log10(p_value) * es_sign;
-% 
-% [lme_MK,md_MK] = fixmm(MK_samples_planes,DMSO_samples_planes);
-% p_value = lme_MK.Coefficients.pValue(3);
-% es_sign = sign(lme_MK.Coefficients.Estimate(3));
-% p_log_MK = -1 * log10(p_value) * es_sign;
-% 
-% [lme_MKJNJ,md_MKJNJ] = fixmm(MKJNJ_samples_planes,MK_samples_planes);
-% p_value = lme_MKJNJ.Coefficients.pValue(3);
-% es_sign = sign(lme_MKJNJ.Coefficients.Estimate(3));
-% p_log_MKJNJ = -1 * log10(p_value) * es_sign;
-% 
-% [lme_MKTAK,md_MKTAK] = fixmm(MKTAK_samples_planes,MK_samples_planes);
-% p_value = lme_MKTAK.Coefficients.pValue(3);
-% es_sign = sign(lme_MKTAK.Coefficients.Estimate(3));
-% p_log_MKTAK = -1 * log10(p_value) * es_sign;
-% 
-% pheatmap_matrix = [p_log_JNJ,p_log_TAK,p_log_MK,p_log_MKJNJ,p_log_MKTAK];
-% % clustergram(pheatmap_matrix_DMSO)
-
 %% all funcs
-
 function [int_ven,int_dor,int_ven_vect,int_dor_vect] = sortvd(locs,int)
     sample_size = size(locs,2);
     int_p2_ven = []; int_p3_ven = []; int_p4_ven = [];
