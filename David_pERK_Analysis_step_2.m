1b5d231e2273e013e5a16b9f6d6c6e19a66f31e4
davidkow9999 <teck.kow@monash.edu>
2025-04-14 14:39:09 +0800

    Update David_pERK_Analysis_step_2.m

--- David_pERK_Analysis_step_2.m
+++ David_pERK_Analysis_step_2.m
@@ -34,13 +34,13 @@ for i = 1:sample_size
     xyz_locs_terk = [NaN,NaN,NaN];
     xyz_locs_perk = [NaN,NaN,NaN];
     exp_date = [];
-    for j = 1:2:5*2 % collecting for all plane
+    for j = 1:2:5*2 % collecting for all plane, one sample
         current_trial = current_group_path + "/" + group_1(j).name; % load for each plane, terk
         terk = readmatrix(current_trial);
         nan_row = any(isnan(terk),2);
         terk_without_nan = terk(~nan_row,:);
         terk_matrix = [terk_matrix; terk_without_nan]; % remove nan
-        mean_aplane_terk = mean(terk_without_nan(:,3),'all');
+        mean_aplane_terk = mean(terk_without_nan(:,3),'all'); % used to calculate ratio
 
         xyz_locs_terk = [xyz_locs_terk;terk_without_nan(:,5),terk_without_nan(:,6),ones(size(terk_without_nan,1),1)*k]; % xyz locations
 
@@ -68,9 +68,11 @@ for i = 1:sample_size
         DMSO_byplane_perk_terk{i} = [aplane_perk_terk{1};aplane_perk_terk{2};aplane_perk_terk{3};
             aplane_perk_terk{4};aplane_perk_terk{5}];
 
+        % by sample by plane
         DMSO_samples_planes{i,1} = aplane_perk_terk{1}; DMSO_samples_planes{i,2} = aplane_perk_terk{2}; DMSO_samples_planes{i,3} = aplane_perk_terk{3}; 
         DMSO_samples_planes{i,4} = aplane_perk_terk{4}; DMSO_samples_planes{i,5} = aplane_perk_terk{5}; 
     
+        % by plane, all samples
         plane1 = [plane1;aplane_perk_terk{1}]; 
         plane2 = [plane2;aplane_perk_terk{2}];
         plane3 = [plane3;aplane_perk_terk{3}];
@@ -79,6 +81,7 @@ for i = 1:sample_size
         DMSO_planes{1} = plane1; DMSO_planes{2} = plane2; DMSO_planes{3} = plane3; %need to redo this, all wrong
         DMSO_planes{4} = plane4; DMSO_planes{5} = plane5;
 
+        % by samples, plane recorded in third column
         DMSO_xyz{i,1} = xyz_locs_terk(2:end,:);
         DMSO_xyz{i,2} = xyz_locs_perk(2:end,:);
 
@@ -197,6 +200,124 @@ for i = 1:sample_size
 end
 end
 
+%% cumulative distribution
+% _samples_planes, by sample and by plane
+% _planes, by plane, all samples 
+
+% DMSO_all_pt = []; TAK_all_pt = []; JNJ_all_pt = []; MK_all_pt = []; MKTAK_all_pt = []; MKJNJ_all_pt = [];
+% group = {DMSO_samples_planes, JNJ_samples_planes, TAK_samples_planes, MK_samples_planes, MKJNJ_samples_planes, MKTAK_samples_planes};
+% for j = 1:size(group,2)
+%     m = 1;
+%     a_group_all_pt = [];
+%     for k = 1:size(group{j},1)
+%         for l = 1:5
+%             a_sample_a_plane = mean(group{j}{k,l},'all');
+%             a_group_all_pt = [a_group_all_pt,a_sample_a_plane];
+%             m = m + 1;
+%         end
+%     end
+%     groups_all_pt{j} = a_group_all_pt;
+% end
+
+% JNJ n TAK
+DMSO_all_pt = [DMSO_planes{1};DMSO_planes{2};DMSO_planes{3};DMSO_planes{4};DMSO_planes{5}];
+JNJ_all_pt = [JNJ_planes{1};JNJ_planes{2};JNJ_planes{3};JNJ_planes{4};JNJ_planes{5}];
+TAK_all_pt = [TAK_planes{1};TAK_planes{2};TAK_planes{3};TAK_planes{4};TAK_planes{5}];
+jnjtak_group_all_pt = {DMSO_all_pt,JNJ_all_pt,TAK_all_pt};
+
+cs_data = [DMSO_all_pt;JNJ_all_pt;TAK_all_pt];
+cs_group = [ones(1,length(DMSO_all_pt))*1, ones(1,length(JNJ_all_pt))*2, ones(1,length(TAK_all_pt))*3];
+[p_dmso_cs,tbl_dmso_cs,stats_dmso_cs] = anova1(cs_data',cs_group,'off');
+p_multcomp_dmso_cs = multcompare(stats_dmso_cs,"CriticalValueType","bonferroni");
+
+% figure();
+% for g = 1:3
+% sort_all_pt = sort(jnjtak_group_all_pt{g},'ascend');
+% one_cd = cumsum(sort_all_pt);
+% cumulative_frac = one_cd ./ one_cd(end);
+% norm_cf = normalize(cumulative_frac,'range',[0 1]);
+% plot(sort_all_pt,norm_cf); hold on
+% end
+
+% MKJNJ n MKTAK
+MK_all_pt = [MK_planes{1};MK_planes{2};MK_planes{3};MK_planes{4};MK_planes{5}];
+MKJNJ_all_pt = [MKJNJ_planes{1};MKJNJ_planes{2};MKJNJ_planes{3};MKJNJ_planes{4};MKJNJ_planes{5}];
+MKTAK_all_pt = [MKTAK_planes{1};MKTAK_planes{2};MKTAK_planes{3};MKTAK_planes{4};MKTAK_planes{5}];
+mkjnjmktak_group_all_pt = {MK_all_pt,MKJNJ_all_pt,MKTAK_all_pt};
+
+mk_cs_data = [MK_all_pt;MKJNJ_all_pt;MKTAK_all_pt];
+mk_cs_group = [ones(1,length(MK_all_pt))*1, ones(1,length(MKJNJ_all_pt))*2, ones(1,length(MKTAK_all_pt))*3];
+[p_mk_cs,tbl_mk_cs,stats_mk_cs] = anova1(mk_cs_data',mk_cs_group,'off');
+p_multcomp_mk_cs = multcompare(stats_mk_cs,"CriticalValueType","bonferroni");
+
+% figure();
+% for g = 1:3
+% sort_all_pt = sort(mkjnjmktak_group_all_pt{g},'ascend');
+% one_cd = cumsum(sort_all_pt);
+% cumulative_frac = one_cd ./ one_cd(end);
+% norm_cf = normalize(cumulative_frac,'range',[0 1]);
+% plot(sort_all_pt,norm_cf); hold on
+% end
+
+%% mean of intensity based on the x-coordinates
+group_x_coor = {DMSO_xyz,JNJ_xyz,TAK_xyz,MK_xyz,MKJNJ_xyz,MKTAK_xyz};
+group_int = {DMSO_samples_planes,JNJ_samples_planes,TAK_samples_planes,MK_samples_planes,MKJNJ_samples_planes,MKTAK_samples_planes};
+for o = 1:size(group_x_coor,2)
+    for p = 1:size(group_x_coor{o},1) % sample
+        for u = 2:4
+            plane_x = find(group_x_coor{o}{p,2}(:,3) == u);
+            x_coor_round_half = ceil((group_x_coor{o}{p,2}(plane_x,1) * 5)) / 5;
+%             x_coor_round_half = round(group_x_coor{o}{p,2}(plane_x,1));            
+            unique_x_coor = unique(x_coor_round_half);
+            sample_int = zeros(length(unique_x_coor));
+            int_by_point = 0;
+            x_length = 1:0.2:7; % change after changing x_coor_round_half!!
+            for i = 1:length(x_length)
+                ind_zero_one = x_coor_round_half == x_length(i);
+                ind_by_point = find(ind_zero_one == 1);
+
+                if all(ind_by_point == 0)
+                    int_by_point(i) = 0;
+                else
+                    sam_pl_int = group_int{o}{p,u};
+                    int_by_point(i) = mean(sam_pl_int(ind_by_point),'all');
+                end
+            end
+            int_by_xcoor_byplane{u-1} = movmean(int_by_point,6);
+        end
+        int_by_xcoor_bysample{p} = int_by_xcoor_byplane;
+    end
+    allsamp_same_plane_two = [zeros(1,length(x_length))];allsamp_same_plane_three = [zeros(1,length(x_length))];allsamp_same_plane_four = [zeros(1,length(x_length))];
+    for p = 1:size(group_x_coor{o},1)
+        allsamp_same_plane_two = [allsamp_same_plane_two; int_by_xcoor_bysample{p}{1}]; 
+        allsamp_same_plane_three = [allsamp_same_plane_three; int_by_xcoor_bysample{p}{2}]; 
+        allsamp_same_plane_four = [allsamp_same_plane_four; int_by_xcoor_bysample{p}{3}];
+    end
+    group_int_by_xcoor{o,1} = allsamp_same_plane_two(2:end,:);
+    group_int_by_xcoor{o,2} = allsamp_same_plane_three(2:end,:);
+    group_int_by_xcoor{o,3} = allsamp_same_plane_four(2:end,:);
+end
+
+% group_int_by_xcoor, row = group, column = plane 2 3 4
+for o = 1:size(group_int_by_xcoor,1)
+    for p = 1:3 % plane
+        one_line = group_int_by_xcoor{o,p};
+        mean_xcoor{o,p} = mean(one_line,1); % zscore
+        std_xcoor{o,p} = std(one_line,[],1);
+        sem_xcoor{o,p} = std_xcoor{o,p} ./ sqrt(length(std_xcoor{o,p}));
+    end
+end
+
+choose_this_mean = mean_xcoor{6,1};
+choose_this_sem = sem_xcoor{6,1};
+x_coor_length = x_length;
+figure();
+plot(x_coor_length,choose_this_mean,'LineWidth',1); hold on;
+fill([x_coor_length, fliplr(x_coor_length)], [choose_this_mean + choose_this_sem, fliplr(choose_this_mean - choose_this_sem)], 'b', 'FaceAlpha', 0.2, 'EdgeColor', 'none');
+
+
+return
+
 %% Identify ventral and dorsal
 [int_ven_DMSO,int_dor_DMSO,iv_d_all,id_d_all] = sortvd(DMSO_xyz,DMSO_samples_planes);
 [int_ven_JNJ,int_dor_JNJ,iv_j_all,id_j_all] = sortvd(JNJ_xyz,JNJ_samples_planes);
@@ -204,6 +325,51 @@ end
 [int_ven_MK,int_dor_MK,iv_m_all,id_m_all] = sortvd(MK_xyz,MK_samples_planes);
 [int_ven_MKJNJ,int_dor_MKJNJ,iv_mj_all,id_mj_all] = sortvd(MKJNJ_xyz,MKJNJ_samples_planes);
 [int_ven_MKTAK,int_dor_MKTAK,iv_mt_all,id_mt_all] = sortvd(MKTAK_xyz,MKTAK_samples_planes);
+
+% LMM - v vs d
+% [lme_vd_DMSO,md_vd_DMSO] = fixmm(int_ven_DMSO,int_dor_DMSO);
+% p_value = lme_vd_DMSO.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_DMSO.Coefficients.Estimate(3));
+% p_log_vd_DMSO = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_JNJ,md_vd_JNJ] = fixmm(int_ven_JNJ,int_dor_JNJ);
+% p_value = lme_vd_JNJ.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_JNJ.Coefficients.Estimate(3));
+% p_log_vd_JNJ = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_TAK,md_vd_TAK] = fixmm(int_ven_TAK,int_dor_TAK);
+% p_value = lme_vd_TAK.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_TAK.Coefficients.Estimate(3));
+% p_log_vd_TAK = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_MK,md_vd_MK] = fixmm(int_ven_MK,int_dor_MK);
+% p_value = lme_vd_MK.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_MK.Coefficients.Estimate(3));
+% p_log_vd_MK = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_MKJNJ,md_vd_MKJNJ] = fixmm(int_ven_MKJNJ,int_dor_MKJNJ);
+% p_value = lme_vd_MKJNJ.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_MKJNJ.Coefficients.Estimate(3));
+% p_log_vd_MKJNJ = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_MKTAK,md_vd_MKTAK] = fixmm(int_ven_MKTAK,int_dor_MKTAK);
+% p_value = lme_vd_MKTAK.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_MKTAK.Coefficients.Estimate(3));
+% p_log_vd_MKTAK = -1 * log10(p_value) * es_sign;
+% 
+% figure('Name','Ventral vs dorsal difference at stimulated condition', 'visible','on'); clf;
+% plot([p_log_vd_DMSO,p_log_vd_JNJ,p_log_vd_TAK,p_log_vd_MK,p_log_vd_MKJNJ,p_log_vd_MKTAK],'LineWidth',1.5,'Color','blue','Marker','.','MarkerSize',15); hold on;
+% xticks([1,2,3,4,5,6]);
+% xticklabels({'DMSO','JNJ','TAK','MK','MKJNJ','MKTAK'});
+% xlim([0.5 6.5]);
+% ylim([0 24]);
+% ylabel('-log_{10}(p)','Interpreter', 'tex')
+% ax = gca;
+% ax.XAxis.FontSize = 13;
+% ax.YAxis.FontSize = 13;
+% ax.XAxis.LineWidth = 1.5;
+% ax.YAxis.LineWidth = 1.5;
+% ax.Box = "off";
 
 %% ven vs dorsal within group
 [h_d,p_d,~,stats_d] = ttest2(iv_d_all,id_d_all); 
@@ -339,6 +505,22 @@ test_group = [ones(1,length(id_m_all))*1, ones(1,length(id_mj_all))*2, ones(1,le
 [p_dor_mjt,tbl_dor_mjt,stats_dor_mjt] = anova1(test_data,test_group','off');
 p_dor_mjt_bon = multcompare(stats_dor_mjt,"CriticalValueType","bonferroni");
 
+return
+
+% two-way anova (CANNOT because of perfect multicolinearity)
+% % DMSO JNJ TAK MK
+% % ventral
+% test_data_DMSO = [iv_d_all;iv_j_all;iv_t_all;iv_m_all];
+% test_group_DMSO = [ones(1,length(iv_d_all))*1, ones(1,length(iv_j_all))*2, ones(1,length(iv_t_all))*3, ones(1,length(iv_m_all))*4];
+% obj1_leng = sum([double(length(iv_d_all)),double(length(iv_j_all)),double(length(iv_t_all))]);
+% test_factor_DMSO(1:length(iv_d_all)) = "normal";
+% test_factor_DMSO(length(iv_d_all)+1:obj1_leng) = "enhance";
+% test_factor_DMSO(obj1_leng+1:obj1_leng+length(iv_m_all)) = "impair";
+% [~,~,stats_DMSO] = anovan(test_data_DMSO,{test_group_DMSO test_factor_DMSO},"Model",2, ...
+%     "Varnames",["test_group_DMSO", "test_factor_DMSO"]);
+% [results,~,~,gnames] = multcompare(stats_DMSO,"Dimension",[1 2]);
+
+
 % ven
 % [f_v_d,bc_v_d] = boxplot3(iv_d_all,iv_j_all,iv_t_all,'DMSO','JNJ','TAK','pERK/tERK ven DMSO'); hold on;
 % ylim([0 4])
@@ -393,7 +575,128 @@ p_dor_mjt_bon = multcompare(stats_dor_mjt,"CriticalValueType","bonferroni");
 % plot([1.1,1.9],[3.5 3.5],'LineWidth',1.5,'Color',[0 0 0]);
 % text(1.3,3.55,'***','FontSize',24,'FontWeight','bold');
 
+
+
+% %% Linear mixed model - ven vs dor
+% % ventral
+% [lme_d_JNJ,md_v_JNJ] = fixmm(int_ven_JNJ,int_ven_DMSO);
+% p_value = lme_d_JNJ.Coefficients.pValue(3);
+% es_sign = sign(lme_d_JNJ.Coefficients.Estimate(3));
+% p_log_v_JNJ = -1 * log10(p_value) * es_sign;
+% 
+% [lme_d_TAK,md_v_TAK] = fixmm(int_ven_TAK,int_ven_DMSO);
+% p_value = lme_d_TAK.Coefficients.pValue(3);
+% es_sign = sign(lme_d_TAK.Coefficients.Estimate(3));
+% p_log_v_TAK = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_MK,md_v_MK] = fixmm(int_ven_MK,int_ven_DMSO);
+% p_value = lme_vd_MK.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_MK.Coefficients.Estimate(3));
+% p_log_v_MK = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_MKJNJ,md_v_MKJNJ] = fixmm(int_ven_MKJNJ,int_ven_MK);
+% p_value = lme_vd_MKJNJ.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_MKJNJ.Coefficients.Estimate(3));
+% p_log_v_MKJNJ = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_MKTAK,md_v_MKTAK] = fixmm(int_ven_MKTAK,int_ven_MK);
+% p_value = lme_vd_MKTAK.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_MKTAK.Coefficients.Estimate(3));
+% p_log_v_MKTAK = -1 * log10(p_value) * es_sign;
+% 
+% % dorsal
+% [lme_d_JNJ,md_d_JNJ] = fixmm(int_dor_JNJ,int_dor_DMSO);
+% p_value = lme_d_JNJ.Coefficients.pValue(3);
+% es_sign = sign(lme_d_JNJ.Coefficients.Estimate(3));
+% p_log_d_JNJ = -1 * log10(p_value) * es_sign;
+% 
+% [lme_d_TAK,md_d_TAK] = fixmm(int_dor_TAK,int_dor_DMSO);
+% p_value = lme_d_TAK.Coefficients.pValue(3);
+% es_sign = sign(lme_d_TAK.Coefficients.Estimate(3));
+% p_log_d_TAK = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_MK,md_d_MK] = fixmm(int_dor_MK,int_dor_DMSO);
+% p_value = lme_vd_MK.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_MK.Coefficients.Estimate(3));
+% p_log_d_MK = -1 * log10(p_value) * es_sign;
+% 
+% [lme_vd_MKJNJ,md_d_MKJNJ] = fixmm(int_dor_MKJNJ,int_dor_MK);
+% p_value = lme_vd_MKJNJ.Coefficients.pValue(3);
+% es_sign = sign(lme_vd_MKJNJ.Coefficients.Estimate(3));
+% p_log_d_MKJNJ = -1 * log10(p_value) * es_sign;
+% 
+% [lme_d_MKTAK,md_d_MKTAK] = fixmm(int_dor_MKTAK,int_dor_MK);
+% p_value = lme_d_MKTAK.Coefficients.pValue(3);
+% es_sign = sign(lme_d_MKTAK.Coefficients.Estimate(3));
+% p_log_d_MKTAK = -1 * log10(p_value) * es_sign;
+% 
+% cg_mat = [...
+%     p_log_v_JNJ,p_log_d_JNJ;...
+%     p_log_v_TAK,p_log_d_TAK;...
+%     p_log_v_MK,p_log_d_MK;...
+%     p_log_v_MKJNJ,p_log_d_MKJNJ;...
+%     p_log_v_MKTAK,p_log_d_MKTAK;];
+% 
+% cg = clustergram(cg_mat);
+% cg.ColumnLabels = {'ventral','dorsal'};
+% cg.RowLabels = {'JNJ','TAK','MK','MKJNJ','MKTAK'};
+% cg.DisplayRange = 3;
+% cg.ShowDendrogram = 'off';
+% 
+% %% Linear mixed model -- habenula as a whole
+% % DMSO_pt_all = [DMSO_perk_terk{1};DMSO_perk_terk{2};DMSO_perk_terk{3}];
+% % MK_pt_all = [MK_perk_terk{1};MK_perk_terk{2};MK_perk_terk{3}];
+% % MKJNJ_pt_all = [MKJNJ_perk_terk{1};MKJNJ_perk_terk{2};MKJNJ_perk_terk{3}];
+% % MKTAK_pt_all = [MKTAK_perk_terk{1};MKTAK_perk_terk{2};MKTAK_perk_terk{3}];
+% % TAK_pt_all = [TAK_perk_terk{1};TAK_perk_terk{2};TAK_perk_terk{3}];
+% % JNJ_pt_all = [JNJ_perk_terk{1};JNJ_perk_terk{2};JNJ_perk_terk{3}];
+% % 
+% % beta_MK = mean(MK_pt_all,'all') - mean(DMSO_pt_all,'all');
+% % beta_MKTAK = mean(MKJNJ_pt_all,'all') - mean(DMSO_pt_all,'all');
+% % beta_MKJNJ = mean(MKTAK_pt_all,'all') - mean(DMSO_pt_all,'all');
+% % beta_TAK = mean(JNJ_pt_all,'all') - mean(DMSO_pt_all,'all');
+% % beta_JNJ = mean(TAK_pt_all,'all') - mean(DMSO_pt_all,'all');
+% % 
+% % neu_int = [DMSO_pt_all;MK_pt_all;MKJNJ_pt_all;MKTAK_pt_all;JNJ_pt_all;TAK_pt_all];
+% % grp_varia = [ones(1,length(DMSO_pt_all))*1,ones(1,length(MK_pt_all))*2,ones(1,length(MKJNJ_pt_all))*3,...
+% %     ones(1,length(MKTAK_pt_all))*4,ones(1,length(TAK_pt_all))*5,ones(1,length(JNJ_pt_all))*6];
+% % 
+% % grp_beta = [ones(1,length(DMSO_pt_all))*1,ones(1,length(MK_pt_all))*beta_MK,ones(1,length(MKJNJ_pt_all))*beta_MKJNJ,...
+% %     ones(1,length(MKTAK_pt_all))*beta_MKTAK,ones(1,length(TAK_pt_all))*beta_TAK,ones(1,length(JNJ_pt_all))*beta_JNJ];
+% % 
+% % grp_date = [ones(1,length(DMSO_pt_all))*1,ones(1,length(MK_pt_all))*2,ones(1,length(MKJNJ_pt_all))*3,...
+% %     ones(1,length(MKTAK_pt_all))*3,ones(1,length(TAK_pt_all))*4,ones(1,length(JNJ_pt_all))*1];
+% 
+% [lme_JNJ,md_JNJ] = fixmm(JNJ_samples_planes,DMSO_samples_planes);
+% p_value = lme_JNJ.Coefficients.pValue(3);
+% es_sign = sign(lme_JNJ.Coefficients.Estimate(3));
+% p_log_JNJ = -1 * log10(p_value) * es_sign;
+% 
+% [lme_TAK,md_TAK] = fixmm(TAK_samples_planes,DMSO_samples_planes);
+% p_value = lme_TAK.Coefficients.pValue(3);
+% es_sign = sign(lme_TAK.Coefficients.Estimate(3));
+% p_log_TAK = -1 * log10(p_value) * es_sign;
+% 
+% [lme_MK,md_MK] = fixmm(MK_samples_planes,DMSO_samples_planes);
+% p_value = lme_MK.Coefficients.pValue(3);
+% es_sign = sign(lme_MK.Coefficients.Estimate(3));
+% p_log_MK = -1 * log10(p_value) * es_sign;
+% 
+% [lme_MKJNJ,md_MKJNJ] = fixmm(MKJNJ_samples_planes,MK_samples_planes);
+% p_value = lme_MKJNJ.Coefficients.pValue(3);
+% es_sign = sign(lme_MKJNJ.Coefficients.Estimate(3));
+% p_log_MKJNJ = -1 * log10(p_value) * es_sign;
+% 
+% [lme_MKTAK,md_MKTAK] = fixmm(MKTAK_samples_planes,MK_samples_planes);
+% p_value = lme_MKTAK.Coefficients.pValue(3);
+% es_sign = sign(lme_MKTAK.Coefficients.Estimate(3));
+% p_log_MKTAK = -1 * log10(p_value) * es_sign;
+% 
+% pheatmap_matrix = [p_log_JNJ,p_log_TAK,p_log_MK,p_log_MKJNJ,p_log_MKTAK];
+% % clustergram(pheatmap_matrix_DMSO)
+
 %% all funcs
+
 function [int_ven,int_dor,int_ven_vect,int_dor_vect] = sortvd(locs,int)
     sample_size = size(locs,2);
     int_p2_ven = []; int_p3_ven = []; int_p4_ven = [];
